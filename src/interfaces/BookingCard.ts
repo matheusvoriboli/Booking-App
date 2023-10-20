bfaabffe8043b7eab54a8db9707b134e13537e91
Matheus Riboli <CX-Matheus.Riboli@glhomes.com>
2023-10-20 18:45:51 -0300

    feature: Initial structure and initial creation of Bookings

---
+++ src/interfaces/BookingCard.ts
@@ -0,0 +1,5 @@
+export interface IBooking {
+   location: string;
+   initialDate: string;
+   finalDate: string;
+ }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['var(--font-lexend)', 'sans-serif'],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#40CAA1",
+        secondary: "#2D2AA5",
+        "primary-dark": "#19172A",
+        "soft-gray": "#F6F7FB"
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
